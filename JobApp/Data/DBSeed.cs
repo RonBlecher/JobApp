2b9f83612307704678906e0d24c0c166c58e1197
RonBlecher <rgorsb@gmail.com>
2020-10-10 13:06:54 +0300

    add SaveChanges

--- JobApp/Data/DBSeed.cs
+++ JobApp/Data/DBSeed.cs
@@ -12,12 +12,26 @@ namespace JobApp.Data
         {
             if (context.Admin.Any(a => a.Name == "admin") == false)
             {
-                context.Admin.Add(new Admin { 
+                context.Admin.Add(new Admin
+                {
                     Name = "admin", 
                     Email = "admin@mail.com", 
                     PhoneNum = "050-0055555", 
                     Password = "123456" 
                 });
+                context.SaveChanges();
+            }
+
+            if (context.Seeker.Any(a => a.Name == "seek") == false)
+            {
+                context.Seeker.Add(new Seeker
+                {
+                    Name = "seek",
+                    Email = "seek@mail.com",
+                    PhoneNum = "050-0055555",
+                    Password = "123456"
+                });
+                context.SaveChanges();
             }
         }
     }
