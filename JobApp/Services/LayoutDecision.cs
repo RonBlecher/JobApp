fcfd3be7e1b093893f1b6a706339dbb1295d04fc
eladisraeli <elad.israeli@nielsen.com>
2020-11-03 00:40:27 +0200

    added way to decide what layout to choose according to user role

---
+++ JobApp/Services/LayoutDecision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+
+
+public class LayoutDecision
+{
+    public static string LocateLayout(ClaimsPrincipal User)
+    {
+        string Layout = "";
+
+        var identity = (ClaimsIdentity)User.Identity;
+        IEnumerable<Claim> claims = identity.Claims;
+        Claim roleClaim = claims.Where(claim => claim.Type == ClaimTypes.Role).First();
+        if (roleClaim.Value == "Seeker")
+        {
+            Layout = "_Layout_seekers";
+
+        }
+        else if (roleClaim.Value.ToString() == "Publisher")
+        {
+            Layout = "_Layout_publishers";
+
+        }
+        else if (roleClaim.Value.ToString() == "Admin")
+        {
+            Layout = "_Layout_admins";
+        }
+
+        return Layout;
+    }
+}
